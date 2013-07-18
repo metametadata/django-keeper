9fd64be403f73ac73512d87a1ee0975cbb77b4f7
yuri <yuri.govor@gmail.com>
2013-07-18 16:33:49 +0000

    fix settings configuration

--- keeper/wsgi.py
+++ keeper/wsgi.py
@@ -13,6 +13,13 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+import os
+
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "keeper.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keeper.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
